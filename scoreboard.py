7fd34de01128a639a13d73be595cc61268e66475
georgianapal807 <111218330+georgianapal807@users.noreply.github.com>
2024-10-14 10:59:18 +0300

    Add files via upload
    Adding highscore from file

--- scoreboard.py
+++ scoreboard.py
@@ -2,8 +2,11 @@ from turtle import Turtle
 from snake import Snake
 from food import Food
 
+
+
 ALIGNMENT = "center"
 FONT = ('Arial', 20, 'normal')
+
 class Scoreboard(Turtle):
 
     def __init__(self):
@@ -14,16 +17,26 @@ class Scoreboard(Turtle):
         self.goto(0,270)
         self.hideturtle()
         self.score = 0
+        with open("data.txt") as data:
+            self.highscore = int(data.read())
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score} High score: {self.highscore}", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score +=1
-        self.clear()
         self.update_scoreboard()
 
-    def game_over(self):
-        self.goto(0,0)
-        self.write("GAME OVER!", align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.highscore:
+            self.highscore = self.score
+            with open("data.txt", mode='w') as data:
+                data.write(f"{self.highscore}")
+        self.score = 0
+        self.update_scoreboard()
+
+    # def game_over(self):
+    #     self.goto(0,0)
+    #     self.write("GAME OVER!", align=ALIGNMENT, font=FONT)
